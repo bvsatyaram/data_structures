471f1b4d8f2cf38054cbc4fcf7e3ad0111ae9f8e
B V Satyaram <bvsatyaram@gmail.com>
2016-05-12 18:40:30 +0530

    Add Problems

--- linked_list/01-find-nth-node-value.rb
+++ linked_list/01-find-nth-node-value.rb
@@ -1,18 +1,44 @@
-# current_node = Get the head
-# n-1 times
-#   current_node = current_node.next
-# current_node.val
+# Write a function that takes 2 arguments; a linked list (ll) and on integer(n).
+# The function should return the value of the nth node in the linked list.
+#
+# Assume that the linked is in non-empty and is of length at least 'n'
 
 require_relative '00-implement'
 
-ll = LinkedList.new
-(1..100).each do |i|
-  ll.insert(i)
+def value_of_nth_node(ll, n)
+  current_node = ll.head
+  (n-1).times do
+    current_node = current_node.next
+  end
+  return current_node.value
 end
 
-n = 55
-current_node = ll.head
-(n-1).times do
-  current_node = current_node.next
+
+require "minitest/autorun"
+
+class TestMeme < Minitest::Test
+  def setup
+    @long_linked_list = LinkedList.new
+    (1..100).each do |i|
+      @long_linked_list.insert(i)
+    end
+    @short_linked_list = LinkedList.new
+    @short_linked_list.insert(1)
+  end
+
+  def test_first_node_of_long_linked_list
+    assert_equal 100, value_of_nth_node(@long_linked_list, 1)
+  end
+
+  def test_last_node_of_long_linked_list
+    assert_equal 1, value_of_nth_node(@long_linked_list, 100)
+  end
+
+  def test_middle_node_of_long_linked_list
+    assert_equal 46, value_of_nth_node(@long_linked_list, 55)
+  end
+
+  def test_first_node_of_short_linked_list
+    assert_equal 1, value_of_nth_node(@short_linked_list, 1)
+  end
 end
-puts current_node.value
---
+++ linked_list/01A-find-nth-value.rb
@@ -0,0 +1,15 @@
+# Write a function that takes 2 arguments; a linked list (ll) and on integer(n).
+# The function should return the value of the nth node in the linked list.
+#
+# Notice that this problem has no assumptions that we had in the earlier problem.
+
+def value_of_nth_node(ll, n)
+end
+
+
+require "minitest/autorun"
+
+class TestMeme < Minitest::Test
+  def setup
+  end
+end
